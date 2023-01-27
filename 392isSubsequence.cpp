e07c779e3b9bbfaee78776b1a675eb9e696277c0
Dhruv Kabara <40836307+kabradhruv@users.noreply.github.com>
2023-01-27 18:39:15 +0530

    Add files via upload

---
+++ 392isSubsequence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        // p is for string t
+        // q is for string s
+        int p = 0, q = 0;
+        while (true)
+        {
+            if (q >= s.length())
+            {
+                return true;
+            }
+            if (p >= t.length())
+            {
+                return false;
+            }
+            if (s[q] == t[p])
+            {
+                p++;
+                q++;
+            }
+            else
+            {
+                p++;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    // string s = "abc", t = "ahbgdc";
+    // string s = "axc", t = "ahbgdc";
+    string s = "bcd", t = "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubcd";
+    Solution q;
+    cout << q.isSubsequence(s, t);
+    return 0;
+}
